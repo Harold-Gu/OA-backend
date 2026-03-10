2364507f95651e3a7aa0fc96e344d723f9873098
Harold-Gu <guhaoran1216@gmail.com>
2026-03-10 14:18:00 +0000

    init

--- oaauth/models.py
+++ oaauth/models.py
@@ -1,8 +1,80 @@
-from django.db import models
-from django.contrib.auth.models import User
-# Rewrite the user model
+from random import choice
 
-class AbstractUser(AbstractBaseUser, PermissionsMixin):
+from django.db import models
+from django.contrib.auth.models import User, AbstractBaseUser,PermissionsMixin,BaseUserManager
+from django.template.defaultfilters import default
+from django.contrib.auth.hashers import make_password
+
+
+class OAUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user_object(self, realname, email, password, **extra_fields):
+
+        if not realname:
+            raise ValueError("It is necessary to enter your real name.")
+        email = self.normalize_email(email)
+        user = self.model(username=realname, email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, email, password, **extra_fields)
+
+    create_superuser.alters_data = True
+
+
+    def with_perm(
+        self, perm, is_active=True, include_superusers=True, backend=None, obj=None
+    ):
+        if backend is None:
+            backends = auth.get_backends()
+            if len(backends) == 1:
+                backend = backends[0]
+            else:
+                raise ValueError(
+                    "You have multiple authentication backends configured and "
+                    "therefore must provide the `backend` argument."
+                )
+        elif not isinstance(backend, str):
+            raise TypeError(
+                "backend must be a dotted import path string (got %r)." % backend
+            )
+        else:
+            backend = auth.load_backend(backend)
+        if hasattr(backend, "with_perm"):
+            return backend.with_perm(
+                perm,
+                is_active=is_active,
+                include_superusers=include_superusers,
+                obj=obj,
+            )
+        return self.none()
+
+
+# Rewrite the user model
+class UserStatusChoices(models.IntegerChoices):
+    # Activated already
+    ACTIVED = 1
+    UNACTIVE = 2
+    LOCKED = 3
+
+class OAUser(AbstractBaseUser, PermissionsMixin):
     """
     An abstract base class implementing a fully featured User model with
     admin-compliant permissions.
@@ -10,42 +82,22 @@ class AbstractUser(AbstractBaseUser, PermissionsMixin):
     Username and password are required. Other fields are optional.
     """
 
-    username_validator = UnicodeUsernameValidator()
 
-    username = models.CharField(
-        _("username"),
-        max_length=150,
-        unique=True,
-        help_text=_(
-            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
-        ),
-        validators=[username_validator],
-        error_messages={
-            "unique": _("A user with that username already exists."),
-        },
-    )
-    first_name = models.CharField(_("first name"), max_length=150, blank=True)
-    last_name = models.CharField(_("last name"), max_length=150, blank=True)
-    email = models.EmailField(_("email address"), blank=True)
-    is_staff = models.BooleanField(
-        _("staff status"),
-        default=False,
-        help_text=_("Designates whether the user can log into this admin site."),
-    )
-    is_active = models.BooleanField(
-        _("active"),
-        default=True,
-        help_text=_(
-            "Designates whether this user should be treated as active. "
-            "Unselect this instead of deleting accounts."
-        ),
-    )
-    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+    realname = models.CharField(max_length=150,unique=False,)
+    email = models.EmailField(unique=True, blank=False)
+    telephone = models.CharField(max_length=20, blank=True,unique=True)
+    is_staff = models.BooleanField(default=True)
 
-    objects = UserManager()
+    status = models.IntegerChoices(choices = UserStatusChoices,default = UserStatusChoices.UNACTIVE)
+
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = OAUserManager()
 
     EMAIL_FIELD = "email"
-    USERNAME_FIELD = "username"
+
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["email"]
 
     class Meta:
